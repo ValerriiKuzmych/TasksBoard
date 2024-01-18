48662fc892b14b70f4a83d32eaa351cf43032b43
Valerii Kuzmych <kuzmychvalerii@gmail.com>
2024-01-18 17:19:48 +0100

    realising class TaskPrioritySettings

--- src/main/java/entities/entitiesSettings/TaskPrioritySettings.java
+++ src/main/java/entities/entitiesSettings/TaskPrioritySettings.java
@@ -1,5 +1,88 @@
 package entities.entitiesSettings;
 
+import entities.Task;
+import entities.TaskPriority;
+
 public class TaskPrioritySettings {
+
+	public void setTaskPriorityLow(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskPriority(setPriorityLow(changeableTask.getTaskPriority()));
+
+	}
+
+	public void setTaskPriorityMedium(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskPriority(setPriorityMedium(changeableTask.getTaskPriority()));
+
+	}
+
+	public void setTaskPriorityHigh(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskPriority(setPriorityHigh(changeableTask.getTaskPriority()));
+
+	}
+
+	public void setTaskPrioritySuperHigh(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskPriority(setPrioritySuperHigh(changeableTask.getTaskPriority()));
+
+	}
+
+	public TaskPriority setPriorityLow(TaskPriority priority) {
+
+		TaskPriority changeablePriority = priority;
+
+		changeablePriority.setLowPriority(true);
+		changeablePriority.setMediumPriority(false);
+		changeablePriority.setHighPriority(false);
+		changeablePriority.setSuperHighPriority(false);
+
+		return changeablePriority;
+	}
+
+	public TaskPriority setPriorityMedium(TaskPriority priority) {
+
+		TaskPriority changeablePriority = priority;
+
+		changeablePriority.setLowPriority(false);
+		changeablePriority.setMediumPriority(true);
+		changeablePriority.setHighPriority(false);
+		changeablePriority.setSuperHighPriority(false);
+
+		return changeablePriority;
+	}
+
+	public TaskPriority setPriorityHigh(TaskPriority priority) {
+
+		TaskPriority changeablePriority = priority;
+
+		changeablePriority.setLowPriority(false);
+		changeablePriority.setMediumPriority(false);
+		changeablePriority.setHighPriority(true);
+		changeablePriority.setSuperHighPriority(false);
+
+		return changeablePriority;
+	}
+
+	public TaskPriority setPrioritySuperHigh(TaskPriority priority) {
+
+		TaskPriority changeablePriority = priority;
+
+		changeablePriority.setLowPriority(false);
+		changeablePriority.setMediumPriority(false);
+		changeablePriority.setHighPriority(false);
+		changeablePriority.setSuperHighPriority(true);
+
+		return changeablePriority;
+	}
 
 }
--- src/main/java/entities/entitiesSettings/TaskStatusSettings.java
+++ src/main/java/entities/entitiesSettings/TaskStatusSettings.java
@@ -1,5 +1,113 @@
 package entities.entitiesSettings;
 
+import entities.Task;
+import entities.TaskStatus;
+
 public class TaskStatusSettings {
+
+	public void setTaskStatusNew(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskStatus(setStatusNew(changeableTask.getTaskStatus()));
+
+	}
+
+	public void setTaskStatusInProgress(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskStatus(setStatusInProgress(changeableTask.getTaskStatus()));
+
+	}
+
+	public void setTaskStatusUnderReview(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskStatus(setStatusUnderReview(changeableTask.getTaskStatus()));
+
+	}
+
+	public void setTaskStatusCompleted(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskStatus(setStatusCompleted(changeableTask.getTaskStatus()));
+
+	}
+
+	public void setTaskStatusArchived(Task task) {
+
+		Task changeableTask = task;
+
+		changeableTask.setTaskStatus(setStatusArchived(changeableTask.getTaskStatus()));
+
+	}
+
+	public TaskStatus setStatusNew(TaskStatus status) {
+
+		TaskStatus changeableStatus = status;
+
+		changeableStatus.setNewTask(true);
+		changeableStatus.setTaskInProgress(false);
+		changeableStatus.setTaskUnderReview(false);
+		changeableStatus.setTaskCompleted(false);
+		changeableStatus.setTaskArchived(false);
+
+		return changeableStatus;
+	}
+
+	public TaskStatus setStatusInProgress(TaskStatus status) {
+
+		TaskStatus changeableStatus = status;
+
+		changeableStatus.setNewTask(false);
+		changeableStatus.setTaskInProgress(true);
+		changeableStatus.setTaskUnderReview(false);
+		changeableStatus.setTaskCompleted(false);
+		changeableStatus.setTaskArchived(false);
+
+		return changeableStatus;
+	}
+
+	public TaskStatus setStatusUnderReview(TaskStatus status) {
+
+		TaskStatus changeableStatus = status;
+
+		changeableStatus.setNewTask(false);
+		changeableStatus.setTaskInProgress(false);
+		changeableStatus.setTaskUnderReview(true);
+		changeableStatus.setTaskCompleted(false);
+		changeableStatus.setTaskArchived(false);
+
+		return changeableStatus;
+	}
+
+	public TaskStatus setStatusCompleted(TaskStatus status) {
+
+		TaskStatus changeableStatus = status;
+
+		changeableStatus.setNewTask(false);
+		changeableStatus.setTaskInProgress(false);
+		changeableStatus.setTaskUnderReview(false);
+		changeableStatus.setTaskCompleted(true);
+		changeableStatus.setTaskArchived(false);
+
+		return changeableStatus;
+	}
+
+	public TaskStatus setStatusArchived(TaskStatus status) {
+
+		TaskStatus changeableStatus = status;
+
+		changeableStatus.setNewTask(false);
+		changeableStatus.setTaskInProgress(false);
+		changeableStatus.setTaskUnderReview(false);
+		changeableStatus.setTaskCompleted(false);
+		changeableStatus.setTaskArchived(true);
+
+		return changeableStatus;
+	}
 
 }
